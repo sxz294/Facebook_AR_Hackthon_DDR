919de4a7f2501827e9b306a58e1f688294b93f81
陆楠 <aileenlu09@hotmail.com>
2020-06-15 19:08:58 +0800

    link total score to display

---
+++ scripts/score_add_up.js
@@ -0,0 +1,40 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//
+// For projects created with v87 onwards, JavaScript is always executed in strict mode.
+//==============================================================================
+
+// How to load in modules
+const Scene = require('Scene'); 
+const Patches = require('Patches'); // Aileen: dommunicate with Patch Editor
+
+// Use export keyword to make a symbol available in scripting debug console
+export const Diagnostics = require('Diagnostics');
+var counterNumber = Scene.root.find('number'); // Aileen: find the "number" object in the navigator
+var scoreNumber = Patches.getScalarValue('total_score'); // Aileen: in the patch editor
+
+counterNumber = scoreNumber.toString();
+
+// To use variables and functions across files, use export/import keyword
+// export const animationDuration = 10;
+
+// Use import keyword to import a symbol from another file
+// import { animationDuration } from './script.js'
+
+// To access scene objects
+// const directionalLight = Scene.root.find('directionalLight0');
+
+// To access class properties
+// const directionalLightIntensity = directionalLight.intensity;
+
+// To log messages to the console
+// Diagnostics.log('Console message logged from the script.');
